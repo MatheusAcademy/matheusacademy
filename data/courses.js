0d58b04db6adc49ce05e9ac0bbaa502f35705903
Matheus Academy <126015848+MatheusAcademy@users.noreply.github.com>
2026-04-08 12:40:04 -0300

    remove: 'Geopolítica do Século XXI' via admin

--- data/courses.js
+++ data/courses.js
@@ -182,25 +182,6 @@ var MA_COURSES = [
     free: true,
     addedTs: 1775501691536,
     active: true
-  },
-  {
-    coverImg: 'assets/capas/gp21auth.jpg',
-    id: 'gp21auth',
-    name: 'Geopolítica do Século XXI',
-    desc: '',
-    file: 'geopolitica-seculo-xxi.html',
-    cat: 'Negócios',
-    modules: 7,
-    topics: 28,
-    hours: 6,
-    quizzes: 7,
-    salt: 'GP21_HENRY_2026_MASTER',
-    ak: 'gp21auth_auth',
-    storagePrefix: 'gp21_',
-    icon: '🏛️',
-    color: '#5b7fff',
-    addedTs: 1775654788353,
-    active: true
   }
 ];
 var MA_CATS = [
